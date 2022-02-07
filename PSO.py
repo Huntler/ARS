835d27475e42bd670c4beb746777482a0ba023ed
Jonas Leuchtenberger <jonas.leuchtenberger@rwth-aachen.de>
2022-02-07 15:43:59 +0100

    First (working) version

--- PSO.py
+++ PSO.py
@@ -1,5 +1,17 @@
+from ast import Lambda
 import random
+from typing import List
 import numpy as np
+import time
+import matplotlib.pyplot as plt
+
+def get_cmap(n, name='hsv'):
+    '''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct 
+    RGB color; the keyword argument name must be a standard mpl colormap name.'''
+    return plt.cm.get_cmap(name, n)
+
+
+INTEGER_MAX = np.iinfo(np.int(10)).max
 
 
 class Particle:
@@ -11,10 +23,122 @@ class Particle:
         self.vel_x = vel_x
         self.vel_y = vel_y
 
-        self.bench_best = None
+        self.bench_best = -1
 
 
-def PSO(a, b, c, step_max, particles_num, benchmark_func):
+class Swarm:
+    def __init__(self, particles_num: int, params: List[float], x_range: List, y_range: List, max_steps, benchmark_func: Lambda, axis) -> None:
+        self._benchmark_func = benchmark_func
+        self._axis = axis
+
+        self._a = params[0]
+        self._b = params[1]
+        self._c = params[2]
+
+        self._history = []
+        self._max_frames = max_steps
+        self._time = 0
+        self._cmap = get_cmap(particles_num+1)
+
+        # best group position
+        self._group_best_x = np.random.rand()
+        self._group_best_y = np.random.rand()
+        self._best_benchmark_val_group = -1
+
+        self._x_min, self._x_max = x_range[0], x_range[1]
+        self._y_min, self._y_max = y_range[0], y_range[1]
+
+        # randomly initialize position and velocity of particles
+        self._swarm = [Particle(random.uniform(x_range[0], x_range[1]), random.uniform(y_range[0], y_range[1]),
+                                random.uniform(x_range[0], x_range[1]), random.uniform(y_range[0], y_range[1]))
+                       for x in range(particles_num)]
+
+    def update(self, frame_number, lr=0.01) -> None:
+        # pause the animation max frames have been reached
+        if frame_number >= self._max_frames:
+            self._plot()
+            return
+
+        # do some special things on the initial frame
+        if frame_number == 0:
+            _x = [p.pos_x for p in self._swarm]
+            _y = [p.pos_y for p in self._swarm]
+
+            h = []
+            for i, (__x, __y) in enumerate(zip(_x, _y)):
+                __z = self._benchmark_func(__x, __y)
+                h.append((__x, __y, __z))
+
+            self._history.append(h)
+            self._plot()
+            return
+
+        h = []
+        # perform PSO step
+        for i, particle in enumerate(self._swarm):
+            # update personal particle high score
+            benchmark_value = self._benchmark_func(particle.pos_x, particle.pos_y)
+            if benchmark_value <= particle.bench_best or particle.bench_best == -1:
+                particle.bench_best = benchmark_value
+                particle.p_best_x = particle.pos_x
+                particle.p_best_y = particle.pos_y
+
+            # update the group's high score
+            benchmark_value = self._benchmark_func(self._group_best_x, self._group_best_y)
+            if self._best_benchmark_val_group == -1:
+                benchmark_value = self._benchmark_func(particle.pos_x, particle.pos_y)
+                self._group_best_x = particle.pos_x
+                self._group_best_y = particle.pos_y
+
+            elif benchmark_value <= self._best_benchmark_val_group:
+                self._best_benchmark_val_group = benchmark_value
+                self._group_best_x = particle.pos_x
+                self._group_best_y = particle.pos_y
+
+            # update velocity
+            r_1 = np.random.rand()
+            r_2 = np.random.rand()
+            new_vel_x = self._a * particle.vel_x + self._b * r_1 * (particle.p_best_x - particle.pos_x) + self._c * r_2 * (
+                self._group_best_x - particle.pos_x)
+            new_vel_y = self._a * particle.vel_y + self._b * r_1 * (particle.p_best_y - particle.pos_y) + self._c * r_2 * (
+                self._group_best_y - particle.pos_y)
+
+            particle.vel_x = new_vel_x
+            particle.vel_y = new_vel_y
+
+            # and set new position
+            particle.pos_x = particle.pos_x + particle.vel_x
+            particle.pos_y = particle.pos_y + particle.vel_y
+
+            # add to the history
+            z = self._benchmark_func(particle.pos_x, particle.pos_y)
+            h.append((particle.pos_x, particle.pos_y, z))
+        self._history.append(h)
+
+        # plot the particles
+        self._plot()
+
+        # randomly tune the 'a'
+        if self._a >= 0.4 and np.random.rand() > 0.5:
+            self._a -= lr
+
+        # wait some time
+        time.sleep(self._time)
+    
+    def _plot(self):
+        for i in range(len(self._history)-1, 0, -1):
+            for j, part in enumerate(self._history[i]):
+                x, y, z = part
+                if not (x < self._x_min or x > self._x_max):
+                    if not (y < self._y_min or y > self._y_max):
+                        a = (0.6**(len(self._history) - i))
+                        self._axis.scatter3D(x, y, z, c=self._cmap(j), alpha=a, zorder=2)
+        
+        if len(self._history) == 10:
+            self._history.pop(0)
+
+
+def PSO(a, b, c, step_max, particles_num, benchmark_func, axis):
     """
     performs PSO
     :param a: learning constant
@@ -29,9 +153,9 @@ def PSO(a, b, c, step_max, particles_num, benchmark_func):
     k = 1
 
     # best group position
-    group_best_x = None
-    group_best_y = None
-    best_benchmark_val_group = None
+    group_best_x = -1
+    group_best_y = -1
+    best_benchmark_val_group = -1
 
     # randomly initialize position and velocity of particles
     rand_range = 15
@@ -60,9 +184,9 @@ def PSO(a, b, c, step_max, particles_num, benchmark_func):
         r_1 = np.random.rand()
         r_2 = np.random.rand()
         new_vel_x = a * particle.vel_x + b * r_1 * (particle.p_best_x - particle.pos_x) + c * r_2 * (
-                    group_best_x - particle.pos_x)
+            group_best_x - particle.pos_x)
         new_vel_y = a * particle.vel_y + b * r_1 * (particle.p_best_y - particle.pos_y) + c * r_2 * (
-                    group_best_y - particle.pos_y)
+            group_best_y - particle.pos_y)
 
         particle.vel_x = new_vel_x
         particle.vel_y = new_vel_y
@@ -74,6 +198,9 @@ def PSO(a, b, c, step_max, particles_num, benchmark_func):
 
     # PSO loop
     while k <= step_max:
+        x_positions = []
+        y_positions = []
+
         # perform PSO step
         for particle in swarm:
             benchmark_value = benchmark_func(particle.pos_x, particle.pos_y)
@@ -91,14 +218,19 @@ def PSO(a, b, c, step_max, particles_num, benchmark_func):
                 r_1 = np.random.rand()
                 r_2 = np.random.rand()
                 new_vel_x = a * particle.vel_x + b * r_1 * (particle.p_best_x - particle.pos_x) + c * r_2 * (
-                        group_best_x - particle.pos_x)
+                    group_best_x - particle.pos_x)
                 new_vel_y = a * particle.vel_y + b * r_1 * (particle.p_best_y - particle.pos_y) + c * r_2 * (
-                        group_best_y - particle.pos_y)
+                    group_best_y - particle.pos_y)
 
                 particle.vel_x = new_vel_x
                 particle.vel_y = new_vel_y
 
                 particle.pos_x = particle.pos_x + particle.vel_x
                 particle.pos_y = particle.pos_y + particle.vel_y
+
+            x_positions.append(particle.pos_x)
+            y_positions.append(particle.pos_y)
+
+        axis.scatter(x_positions, y_positions)
 
         k += 1
--- main.py
+++ main.py
@@ -1,58 +1,62 @@
+from tkinter import SW
 import numpy as np
 
 import matplotlib.pyplot as plt
 from matplotlib import cm
-import matplotlib.animation as animation
+from matplotlib.animation import FuncAnimation
 
 from benchmark_functions import rosenbrock, rastrigin
 
-from PSO import PSO
-
-PSO(1, 2, 1, 20, 20, rosenbrock)
+from PSO import Swarm
 
 # Fixing random state for reproducibility
 np.random.seed(19680801)
 
 # set initial limits
-x_min, x_max = -2, 2
-y_min, y_max = -0.5, 3
+x_min, x_max = -2.5, 2.5
+y_min, y_max = -1, 3
 z_min, z_max = 0, 2500
 
 # create ranges of x and y
-x = np.arange(x_min, x_max, 0.01)
-y = np.arange(y_min, y_max, 0.01)
+x = np.arange(x_min, x_max, 0.1)
+y = np.arange(y_min, y_max, 0.1)
 x, y = np.meshgrid(x, y)
 
 # load the landscape
 # FIXME: use argv to load the wanted landscape
-z = rosenbrock(x, y)
+z = rosenbrock(x, y, a=0, b=50)
 
 # Attaching 3D axis to the figure
-fig = plt.figure(figsize=(8, 4))
+fig = plt.figure(figsize=(8, 8))
 # FIXME: change title if different function is used
 fig.suptitle("Rastrigin Benchmark")
 
 # plot the 2d plane
-ax2d = fig.add_subplot(1, 2, 1)
-ax2d.imshow(z, cmap=cm.coolwarm)
+# ax2d = fig.add_subplot(1, 2, 1)
+# ax2d.imshow(z, cmap=cm.coolwarm, origin='lower')
 
 # plot the 3d plane
-ax3d = fig.add_subplot(1, 2, 2, projection='3d')
+ax3d = fig.add_subplot(1, 1, 1, projection='3d')
 ax3d.set_xlim3d(x_min, x_max)
 ax3d.set_ylim3d(y_min, y_max)
 ax3d.set_zlim3d(z_min, z_max)
+ax3d.view_init(elev=90, azim=0)
 
 # plot the fitness function
-surf = ax3d.plot_surface(x, y, z, cmap=cm.coolwarm,
-                         linewidth=0, antialiased=False)
+surf = ax3d.plot_surface(x, y, z, cmap=cm.coolwarm, alpha=0.3, linewidth=0, antialiased=False)
+
 fig.colorbar(surf, shrink=0.5, aspect=5)
 
-# Rotate the axes and update
-for angle in range(0, 360, 1):
-    # Normalize the angle to the range [-180, 180] for display
-    angle_norm = (angle + 180) % 360 - 180
-    ax3d.view_init(10, angle_norm)
+# initialize the swarm
+max_iterations = 1000
+swarm = Swarm(20, params=[0.9, 2, 2], x_range=[x_min, x_max], y_range=[
+              y_min, y_max], benchmark_func=rosenbrock, max_steps=max_iterations, axis=ax3d)
 
-    plt.title("3D Visualitzation")
-    plt.draw()
-    plt.pause(.01)
+def update(frame_number):
+    ax3d.clear()
+    swarm.update(frame_number)
+    ax3d.plot_surface(x, y, z, cmap=cm.coolwarm, alpha=0.3, linewidth=0, antialiased=False)
+
+# show the animation and run the simulation using swarm.update
+animation = FuncAnimation(fig, update, interval=1)
+plt.show()
