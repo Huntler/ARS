136ca213d6bef7e667ea7a2085d7d7c899f5dee4
Jonas Leuchtenberger <jonas.leuchtenberger@rwth-aachen.de>
2022-02-07 15:52:04 +0100

    swappable bench-funcs

--- main.py
+++ main.py
@@ -4,18 +4,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib.animation import FuncAnimation
-
 from benchmark_functions import rosenbrock, rastrigin
-
 from PSO import Swarm
+import sys
 
 # Fixing random state for reproducibility
-np.random.seed(19680801)
+np.random.seed(42)
 
-# set initial limits
-x_min, x_max = -2.5, 2.5
-y_min, y_max = -1, 3
-z_min, z_max = 0, 2500
+# read the bench function from user
+bench_name = "rosenbrock"
+if len(sys.argv) == 2:
+    bench_name = sys.argv[1]
+
+if bench_name == "rosenbrock":
+    # set initial limits
+    x_min, x_max = -2.5, 2.5
+    y_min, y_max = -1, 3
+    z_min, z_max = 0, 2500
+    bench = lambda x, y: rosenbrock(x, y, a=0, b=50)
+    title = "Rosenbrock"
+
+elif bench_name == "rastrigin":
+    # set initial limits
+    x_min, x_max = -5, 5
+    y_min, y_max = -5, 5
+    z_min, z_max = 0, 100
+    bench = rastrigin
+    title = "Rastrigin"
+else:
+    print("Only 'rosenbrock' and 'rastigrin' are allowed.")
+    quit()
 
 # create ranges of x and y
 x = np.arange(x_min, x_max, 0.1)
@@ -23,13 +41,12 @@ y = np.arange(y_min, y_max, 0.1)
 x, y = np.meshgrid(x, y)
 
 # load the landscape
-# FIXME: use argv to load the wanted landscape
-z = rosenbrock(x, y, a=0, b=50)
+z = bench(x, y)
 
 # Attaching 3D axis to the figure
 fig = plt.figure(figsize=(8, 8))
 # FIXME: change title if different function is used
-fig.suptitle("Rastrigin Benchmark")
+fig.suptitle(f"{title} Benchmark")
 
 # plot the 2d plane
 # ax2d = fig.add_subplot(1, 2, 1)
@@ -50,7 +67,7 @@ fig.colorbar(surf, shrink=0.5, aspect=5)
 # initialize the swarm
 max_iterations = 1000
 swarm = Swarm(20, params=[0.9, 2, 2], x_range=[x_min, x_max], y_range=[
-              y_min, y_max], benchmark_func=rosenbrock, max_steps=max_iterations, axis=ax3d)
+              y_min, y_max], benchmark_func=bench, max_steps=max_iterations, axis=ax3d)
 
 def update(frame_number):
     ax3d.clear()
